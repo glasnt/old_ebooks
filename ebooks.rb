31e1731b405524fd9d3aa834664aa9cd7ccdd007
Katie McLaughlin <katie@glasnt.com>
2015-12-19 18:59:09 +1100

    Cleanup cleanup stuff, add iterations

--- ebooks.rb
+++ ebooks.rb
@@ -1,33 +1,35 @@
-require 'rubygems'
-require 'thread'
 require 'csv'
 require 'marky_markov'
 
 PATH_TO_TWEETS_CSV   = 'tweets.csv'
 PATH_TO_TWEETS_CLEAN = 'markov_dict.txt'
-PATH_TO_GENERATED    = 'generated.txt'
+
+iterations = (ARGV[0] || 1).to_i
 
 csv_text = CSV.parse(File.read(PATH_TO_TWEETS_CSV))
 
 # Some very basic cleanup of content, with only passing regard for the English language
 def cleanup tweet
-  tweet = tweet
+  tweet
     .gsub(/(?:f|ht)tps?:\/[^\s]+/, '')   # remove links
     .gsub(/\n/,' ')                      # remove new lines
-    .gsub(/\@[\w]*/,'')                  # remove @-mentions
-    .gsub(/ \#[\w]+ /,". ")              # remove hashtags
+    .gsub(/\@[\w]*/,"")                  # remove @-mentions
+    .gsub(/\#[\w]+/,"")                  # remove hashtags
     .gsub("RT",'')                       # remove old-style tweets
     .gsub(/[\s]+/, " ")                  # remove duplicate spaces
     .gsub(/^([\s]+)/, "")                # remove leading space
+end
 
+# Cleanup generated content 
+def punct tweet
   tweet += "." unless tweet[-1] == "."   # end tweet in a full stop
   tweet.capitalize!                      # capitalize
 
-  [".", ":", "!"].each do |x|            # rudimentary punctuation cleanup
+  [":", "!", ","].each do |x|            # rudimentary punctuation cleanup
     tweet.gsub!(/\s#{x}[\s]+([\w])/) { |m| "#{x} #{m[-1].capitalize!}"}
   end
 
-  tweet
+  tweet.match(/^.{0,140}\b/)[0]
 end
 
 # Generate Seed Input
@@ -37,17 +39,13 @@ File.open(PATH_TO_TWEETS_CLEAN, 'w') do |file|
     file.write("#{tweet_text}\n")
   end
 end
- 
+
 # Marky Markov, here with the master plan
 markov = MarkyMarkov::Dictionary.new('dictionary')
 markov.parse_file PATH_TO_TWEETS_CLEAN
-tweet_text = markov.generate_n_sentences(2)
 
-# Truncate output to under 140 characters
-tweet = cleanup tweet_text.match(/^.{0,140}\b/)[0]
+iterations.times do
+  puts punct markov.generate_n_sentences(2)
+end
+
 markov.save_dictionary!
-
-# Save a running log of results
-File.open(PATH_TO_GENERATED, 'a') { |f| f.write("#{tweet}\n") }
-
-puts tweet
